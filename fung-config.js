14dc4d6b648b96999a222021068c93ab2078f045
李子杰 <leechikit0823@163.com>
2017-10-01 17:36:04 +0800

    update

---
+++ fung-config.js
@@ -0,0 +1,18 @@
+{
+    project: {
+        type: "input"
+    },
+    title: {
+        type: "input",
+        defaultValue: "default title"
+    },
+    author:{
+        type: "input",
+        defaultValue: "leechikit"
+    }
+    system: {
+        type: "select",
+        list: ["mobile", "pc"],
+        defaultValue: "mobile"
+    }
+}
