63022724a883d3411eac9a626cfa175e6c0f67bd
李子杰 <leechikit0823@163.com>
2017-10-13 11:15:53 +0800

    update

--- fung-config.js
+++ fung-config.js
@@ -3,20 +3,9 @@
         type: "input",
         message: "请输入项目名："
     },
-    title: {
-        type: "input",
-        message: "请输入标题：",
-        default: "default title"
-    },
     author:{
         type: "input",
         message:"请输入开发者：",
         default: "leechikit"
-    },
-    system: {
-        type: "list",
-        message: "请选择项目类型",
-        choices: ["mobile", "pc"],
-        default: "mobile"
     }
 }
