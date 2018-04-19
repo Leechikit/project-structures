6ff7a5c52b179a2fb1fb1d5df86b697395552ae1
leechikit <leechikit0823@163.com>
2018-04-19 18:22:06 +0800

    update

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './register.js';
-import store from '../vuex/store';
+import store from './vuex/store';
 
 var app = new Vue({
   el: '#app',
